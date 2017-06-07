92f64165ff2292a5e149c526d8e8a075056df1df
luoqiaojoe@163.com <you@example.com>
2017-06-08 01:48:54 +0800

    demo

--- jQuery小例子/打气球（面向对象）/myJQ.js
+++
@@ -1,8 +0,0 @@
-;(function(){
-	function getRdm(m,n){
-
-		return m + Math.round( Math.random()*(n - m) );
-
-	}
-	window.getRdm = getRdm;
-})()
--- jQuery小例子/芒果/js/footer.js
+++
@@ -1,6 +0,0 @@
-	$(function(){
-		$('.soarea-btn').click(function(){
-			$(this).toggleClass('on');
-			$('#J_footer_soarea').slideToggle(150, 'swing');
-		})
-	})
